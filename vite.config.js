edd4caa63469bd8bb2cdc1eba8f7cc8254b3f172
jtabz <jzitko@tableau.com>
2025-02-21 11:11:43 -0600

    Your update message

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tabaf-pov-prompt/',
+  base: '/tabaf-pov-prompt/', // Make sure this matches your repository name exactly
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
