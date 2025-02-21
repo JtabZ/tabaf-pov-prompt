194f57efaabc65741261d168d8b978509661c680
jtabz <jzitko@tableau.com>
2025-02-21 10:43:32 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TemplateEditor from './components/TemplateEditor'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <TemplateEditor />
+    </div>
+  )
+}
+
+export default App
